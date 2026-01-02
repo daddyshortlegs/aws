df6037bdeddd42e41d48243ac2f482bbf31c463a
andys <andysmith836@gmail.com>
2026-01-02 11:32:29 +0000

    Replace deploy.sh scripts with makefiles

--- backend/deploy.sh
+++
@@ -1,3 +0,0 @@
-export ANSIBLE_HOST_KEY_CHECKING=False
-cargo build --target x86_64-unknown-linux-gnu --release
-ansible-playbook -i ../inventory/nodes.yaml deploy-backend.yaml -u andy -K --ask-pass
