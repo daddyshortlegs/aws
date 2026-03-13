b4cc835f5ad7a5d85b17d0794d6e5caed93b15f6
andys <andysmith836@gmail.com>
2026-03-13 22:31:16 +0000

    Add configs for local testing

--- start.sh
+++ start.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 set -m
 
-cd backend && cargo run &
-PID=$!
-echo "Backend running on PID: $PID"
-echo $PID > services.pid
-
 cd proxy && cargo run &
 PID=$!
 echo "Proxy running on PID: $PID"
 echo $PID >> services.pid
+
+cd backend && cargo run &
+PID=$!
+echo "Backend 1 running on PID: $PID"
+echo $PID > services.pid
+
+cd backend && APP_ENV=backend2 cargo run &
+PID=$!
+echo "Backend 2 running on PID: $PID"
+echo $PID > services.pid
+
+cd backend && APP_ENV=backend3 cargo run &
+PID=$!
+echo "Backend 3 running on PID: $PID"
+echo $PID > services.pid
 
 cd node-ssh && npm start &
 PID=$!
